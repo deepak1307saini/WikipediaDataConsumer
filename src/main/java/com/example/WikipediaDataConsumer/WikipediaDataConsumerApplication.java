bf74e21e510331b40673461faf61db06160e8990
Deepak Saini <deepak1307saini@gmail.com>
2023-03-28 13:03:09 +0530

    initialization

---
+++ src/main/java/com/example/WikipediaDataConsumer/WikipediaDataConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.WikipediaDataConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikipediaDataConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikipediaDataConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WikipediaDataConsumer/WikipediaDataConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WikipediaDataConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikipediaDataConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
